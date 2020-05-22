a42076ec83da5521c08d5eaaf93fcd1709f0713d
Kevin Medina <kmedinakm@hotmail.com>
2020-05-21 22:36:45 -0400

    install axios, add onChange handler for search input and onSubmit function to fetch the data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setSearch(value);
+  };
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    let searchLink = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${process.env.API_KEY}`;
+
+    axios
+      .get(searchLink)
+      .then((weather) => {
+        console.log("City info: ", weather.data);
+      })
+      .catch((err) =>
+        console.log("Error while getting weather from the API: ", err)
+      );
+  };
+
   return (
     <div className="search">
       <header className="header">
-        <input
-          type="text"
-          placeholder="Enter City Name"
-          id="search-txt"
-        ></input>
-        <span id="search-btn">
-          <i className="fas fa-search"></i>
-        </span>
+        <form onSubmit={submitSearch}>
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            id="search-txt"
+            value={search}
+            onChange={handleSearch}
+          ></input>
+          <button id="search-btn">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
       </header>
     </div>
   );
